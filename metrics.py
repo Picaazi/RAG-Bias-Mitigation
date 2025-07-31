c6abc1b6ee60bb4291aa3bbc7caa07f5d622e15c
dylan-dk-kim <dylan.dlk.kim@gmail.com>
2025-07-31 17:33:10 -0400

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -3,4 +3,13 @@
 """These functions can and should be used for both decomposition and perturbation methods""" 
 
 def doc_overlap(original_set, new_set): 
-  
+    """
+    Generate a percentage based on document overlap between documents retrived 
+    from original queries vs sub-queries  / rephrased queries 
+    
+    Args:
+        original_set: list of top-k documents retrieved from original queries 
+        new_set: list of top-k documents retrieved from new queries
+    """
+
+    intersect =  original_set.intersection(new_set) 
