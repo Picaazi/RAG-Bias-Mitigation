f7a4e987748b77416cbd2cd05af478f862e1e835
dylan-dk-kim <dylan.dlk.kim@gmail.com>
2025-07-31 17:27:35 -0400

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,3 +1,6 @@
 """Bias Metrics Listed Here: Document Overlap, Semantic Similarity, Representation Variance, Bias Amp Score, etc.""" 
 
-import 
+"""These functions can and should be used for both decomposition and perturbation methods""" 
+
+def doc_overlap(original_set, new_set): 
+  
