1d237c4ebbec23c25933c30e39af2ec13569ec18
Saanvik-ai <saanvikatepalli@gmail.com>
2025-08-16 15:55:45 +0100

    Update data_reading.py

--- src/data_reading.py
+++ src/data_reading.py
@@ -3,24 +3,26 @@
 import pandas as pd
 from datasets import load_dataset
 
-print("Listing contents of GenderBias-QA directory:")
-!ls debiasing-rag/dataset/tasks/GenderBias-QA
+##Gender-biasQA##
 gender_train_df = pd.read_csv("debiasing-rag/dataset/tasks/GenderBias-QA_train.csv")
 gender_test_df = pd.read_csv("debiasing-rag/dataset/tasks/GenderBias-QA_test.csv")
 
-print("Listing contents of PoliticBias-QA directory:")
-!ls debiasing-rag/dataset/tasks/PoliticBias-QA
+##Politic-biasQA##
 politics_train_df = pd.read_csv("debiasing-rag/dataset/tasks/PoliticBias-QA_train.csv")
 politics_test_df = pd.read_csv("debiasing-rag/dataset/tasks/PoliticBias-QA_test.csv")
 
-#BBQ dataset
+##BBQ dataset##
+#git clone https://github.com/nyu-mll/BBQ
 race_df=pd.read_json("BBQ/data/Race_ethnicity.jsonl", lines=True)
 religion_df=pd.read_json("BBQ/data/Religion.jsonl", lines=True)
 genderidentity_df=pd.read_json("BBQ/data/Gender_identity.jsonl", lines=True)
 age_df=pd.read_json("BBQ/data/Age.jsonl", lines=True)
 
+##BibleQA##
 #git clone https://github.com/helen-jiahe-zhao/BibleQA
 bibleQAtrain_df=pd.read_csv("BibleQA/data/bible_qa/bible_qa_train.csv")
+
+##IslamQA##
 dataset = load_dataset("minhalvp/islamqa", split="train")
 df = dataset.to_pandas()
 
