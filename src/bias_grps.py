8e86310f0d9069beb90528fd747236c2989e2fc3
dylan-dk-kim <dylan.dlk.kim@gmail.com>
2025-08-26 22:29:02 -0400

    Update bias_grps.py

--- src/bias_grps.py
+++ src/bias_grps.py
@@ -7,13 +7,13 @@ def get_bias_grps():
 
         "Gender": ["Women", "Men", "Female", "Male", "He", "She", "They", "Transgender", "Queer", "Non-binary", "Him", "Her", 
                    "His", "Hers", "Sir", "Madam", "Mr.", "Ms.", "Mrs.", "Cisgender", "Agender", "Genderqueer", "Genderfluid", 
-                   "Intersex", "Mother", "Father", "Husband", "Wife", "Bride", "Groom", "Them", "Themself", "Masculine", 
+                   "Intersex", "Mother", "Mom", "Dad", "Father", "Husband", "Wife", "Bride", "Groom", "Them", "Themself", "Masculine", 
                    "Feminine"],
 
 
-        "Politics": ["Conservative", "Democrat", "Republican", "Labour", "Left claim", "Right claim", "Communist",
+        "Politics": ["Conservative", "Democrat", "Democratic", "Republican", "Labour", "Left claim", "Right claim", "Communist",
                       "Socialist", "Anarchist", "Fascist", "Progressive", "Liberal", "Libertarian", "Centrist", "Far left", 
-                      "Far right", "Moderate", "Alt-right"],
+                      "Far right", "Moderate", "Alt-right", "Leftist", "Rightist"],
 
 
         "Religion": ["Christianity", "Christian", "Islam", "Muslim", "Judaism", "Jew" "Jewish", "Hinduism", "Hindu",
@@ -22,5 +22,6 @@ def get_bias_grps():
 
 
         "Age": ["Young", "Old", "Teenager", "Young adult", "Middle-aged", "Elderly", "Seniors", "Juniors", "Infant", "Child", 
-                "Baby", "Millenial", "Gen Z", "Gen Alpha", "Boomer", "Youth", "Toddler", "Adolescent", "Adult", "Middle-age", "Senior"]
+                "Baby", "Millenial", "Gen Z", "Gen Alpha", "Boomer", "Youth", "Toddler", "Adolescent", "Adult", "Middle-age", "Senior"
+            , "Elder"]
     }
