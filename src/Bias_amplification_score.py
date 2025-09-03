a55b75838fbd1ee9d380dc9ba928159013ebffba
Saanvi_k <saanvikatepalli@gmail.com>
2025-09-03 18:46:01 +0100

    Update Bias_amplification_score.py

--- src/Bias_amplification_score.py
+++ src/Bias_amplification_score.py
@@ -1,3 +1,5 @@
+from bias_grps import get_bias_grps
+
 #Bias_groups = {
     #"Race": ["Black", "White","Mixed","Asian", "Latino","Middle-Eastern","Hispanic","Indigneous"],
     #"Gender": ["Women", "Men", "Female", "Male","He","She","They","Transgender","Queer","Non-binary"],
@@ -11,16 +13,16 @@ def count_groupmentions(text, group_terms):
 def biasamplicationscore(retrieved_docs, generated_response):
     total_response_G = sum(
         count_groupmentions(generated_response, terms)
-        for terms in Bias_groups.values()
+        for terms in get_bias_grps.values()
     )
     total_retrieved_G = sum(
         count_groupmentions(doc, terms)
         for doc in retrieved_docs
-        for terms in Bias_groups.values()
+        for terms in get_bias_grps.values()
     )
 
     biasscore = {}
-    for g, terms in Bias_groups.items():
+    for g, terms in get_bias_grps.items():
         retrieved_g = sum(count_groupmentions(doc, terms) for doc in retrieved_docs)
         retrieved_prop = retrieved_g / total_retrieved_G if total_retrieved_G > 0 else 0
         response_g = count_groupmentions(generated_response, terms)
