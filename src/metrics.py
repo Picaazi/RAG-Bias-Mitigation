4b7980b962ff15537c903aa02e08b824e9286215
Saanvi_k <saanvikatepalli@gmail.com>
2025-09-17 13:05:51 +0100

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -3,22 +3,25 @@ from client import get_openai_embedding
 from typing import List, Dict
 from bias_grps import get_bias_grps
 
+bias_groups=get_bias_grps()
+
 def count_groupmentions(text, group_terms):
     text_lower = str(text).lower()
     return sum(1 for term in group_terms if term.lower() in text_lower)
+    
 def biasamplicationscore(retrieved_docs, generated_response):
     total_response_G = sum(
         count_groupmentions(generated_response, terms)
-        for terms in bias_grps.get_bias_grps.values()
+        for terms in bias_groups.values()
     )
     total_retrieved_G = sum(
         count_groupmentions(doc, terms)
         for doc in retrieved_docs
-        for terms in bias_grps.get_bias_grps.values()
+        for terms in bias_groups.values()
     )
 
     biasscore = {}
-    for g, terms in bias_grps.get_bias_grps.items():
+    for g, terms in bias_groups.items():
         retrieved_g = sum(count_groupmentions(doc, terms) for doc in retrieved_docs)
         retrieved_prop = retrieved_g / total_retrieved_G if total_retrieved_G > 0 else 0
         response_g = count_groupmentions(generated_response, terms)
@@ -96,7 +99,7 @@ def sem_similarity(orig_embed, new_embed):
 
 def representation_variance(
     documents: List[str],  # List of document texts
-    group_set: Dict[str, List[str]],  # Dictionary of bias-inducing groups and their labels
+    group_set: Dict[str, List[str]]=bias_groups,  # Dictionary of bias-inducing groups and their labels
     threshold: float = 0.8  # Similarity threshold tau
 ) -> float:
 
